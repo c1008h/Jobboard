aaad63f4e8f66795eb15f35ad17bd4f5f6027742
Shema Dabiri <dabirishema@gmail.com>
2023-02-18 13:49:19 -0600

    Revert "initial app commit"
    This reverts commit cfc08e6a863142b0090e3e8bf126c883eb9919ae.

--- server/server.js
+++ server/server.js
@@ -1,22 +1,4 @@
-const express = require('express');
-const session = require('express-session')
-const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 
 const app = express();
-const port = 3000 || process.env.PORT;
-
-const mongoose = require( "mongoose" );
-
-require('dotenv').config();
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/jobboard", 
-{ useNewUrlParser: true, useUnifiedTopology: true});
-
-
-app.listen(port, () => {
-    dbo.connectToServer(function (err) {
-        if(err) console.log(err);
-    });
-    console.log('server is running on port: ${port}');
-});
