c9d825e8619e4597147be089038a925e132e91f2
Shema Dabiri <dabirishema@gmail.com>
2023-02-18 14:08:59 -0600

    back end dependencies and mongoose setup

--- server/server.js
+++ server/server.js
@@ -1,4 +1,21 @@
 const express = require('express')
 const cors = require('cors')
+const session = require("express-session")
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose ');
+const mongoose = require('mongoose');
 
 const app = express();
+
+require('dotenv').config();
+const port = 3000 || process.env.PORT;
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/jobboard', 
+{useNewUrlParser: true, useUnifiedTopology: true});
+
+app.listen(port, ()=> {
+    dbo.connectToServer(function(err){
+        if(err) console.log(err);
+    });
+    console.log('server is running on port ${port}');
+});
