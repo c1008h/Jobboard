3b628a0c15d8aa4ec9de27f4ccead6597dd39e9c
Chris Hong <hongchris97@gmail.com>
2023-02-18 18:24:37 -0800

    Added Job schema in model

---
+++ server/models/Job.js
@@ -0,0 +1,40 @@
+const { Schema } = require('mongoose');
+
+const jobSchema = new Schema ({
+    job_id: {
+        type: String,
+        required: true
+    },
+    employer_name: {
+        type: String,
+    },
+    employer_logo: {
+        type: String
+    },
+    apply_link: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    is_remote: {
+        type: Boolean
+    },
+    posted_date: {
+        type: String
+    },
+    country: {
+        type: String
+    },
+    state: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    offer_expire: {
+        type: String
+    }
+})
+
+module.exports = jobSchema;
