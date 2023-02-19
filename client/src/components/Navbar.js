233645bfacdacef7945d82be241d421604eb0242
Chris Hong <hongchris97@gmail.com>
2023-02-18 19:46:36 -0800

    Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,18 +14,18 @@ export const Navbar = () => {
         <nav className='navbar navbar-expand-lg navbar-light'>
             <Link to='/' id='title' className='col-6'><span id='title'>Jobboard</span></Link>
             <DropdownButton id="dropdown-basic-button" className='col-3' title="More" >
-            {/* {authService.loggedIn()?( */}
+            {authService.loggedIn()?(
 
                 <>
                 <Dropdown.Item as={Link} to="/myprofile">My Profile</Dropdown.Item>
                 <Dropdown.Item as={Link} to="/allfoods" onClick={ logout }>Logout</Dropdown.Item>
                 </>
-            {/* ) : (
+            ) : (
                 <>
                 <Dropdown.Item as={Link} to="/login">Login</Dropdown.Item>
                 <Dropdown.Item as={Link} to="/signup">Signup</Dropdown.Item>
                 </>
-            )} */}
+            )}
             </DropdownButton> 
         </nav>
     )
