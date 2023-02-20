c21065f36f9db38be107b8ac6fee3946c279acfc
Chris Hong <hongchris97@gmail.com>
2023-02-20 02:43:59 -0800

    details of one single saved job

--- client/src/components/savedjobs/OneSavedJob.js
+++ client/src/components/savedjobs/OneSavedJob.js
@@ -4,21 +4,57 @@ import { searchJob } from '../../utils/api';
 
 export const OneSavedJob = () => {
     const { id } = useParams();
-    const [job, setJob] = useState(null);
+    const [job, setJob] = useState({});
 
     useEffect(() => {
-        async function fetchJob() {
-            const result = await searchJob(id);
-            setJob(result);
+        const fetchJob = async () => {
+            try {
+                const data = await searchJob(id)
+                setJob(data[0]);
+            } catch (error) {
+                console.error(error)
+            }
         }
         fetchJob();
     }, [id]);
     console.log(job)
+    const date = new Date(job.job_posted_at_datetime_utc);
+    const formattedDate = date.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
     return (
-        <div>
+        <div style={{padding:'10%', justifyContent:'center', alignItems:'center'}}>
             <h2>{id}</h2>
-            {job[0].employer_logo? <img src={job[0].employer_logo} alt='employer logo'/> : null }
-            <p>Additional content goes here.</p>
+            <div style={{textAlign:'center'}}>
+                {job.employer_logo? 
+                <img src={job.employer_logo} alt='employer logo' style={{height:'300px', width:'80%'}}/> 
+                : null }
+            </div>
+            <p>Employer Website: <a href={job.employer_website}>{job.employer_website}</a></p>
+            <p>{formattedDate}</p>
+            <p>Type: {job.job_employment_type}</p>
+            <p>Remote: {job.job_is_remote? 'True' : 'False'}</p>
+            <p>{job.job_city}, {job.job_state}, {job.job_country}</p>
+            <p>{job.job_highlight}</p>
+            <p>Responsibilities:</p>
+            <ul>
+                {job.job_highlights.Responsibilities.map((responsibilitie, index) => (
+                <li key={index}>{responsibilitie}</li>
+                ))}
+            </ul>
+            <p>Qualifications:</p>
+            <ul>
+                {job.job_highlights.Qualifications.map((qualification, index) => (
+                <li key={index}>{qualification}</li>
+                ))}
+            </ul>
+            <p>Benefits:</p>
+            <ul>
+                <li>{job.job_benefits}</li>
+                {job.job_highlights.Benefits.map((benefit, index) => (
+                <li key={index}>{benefit}</li>
+                ))}
+            </ul> 
+            <button>Apply</button>
+            <button>Remove</button>
         </div>
     )
 }
