ef149808a7e63a6e5aaab41d66b9a395d8ad08a6
Chris Hong <hongchris97@gmail.com>
2023-02-20 01:08:01 -0800

    one saved job renders image

--- client/src/components/savedjobs/OneSavedJob.js
+++ client/src/components/savedjobs/OneSavedJob.js
@@ -17,7 +17,7 @@ export const OneSavedJob = () => {
     return (
         <div>
             <h2>{id}</h2>
-            <img src={job.employer_logo} alt='employer logo'/>
+            <img src={job[0].employer_logo} alt='employer logo'/>
             <p>Additional content goes here.</p>
         </div>
     )
